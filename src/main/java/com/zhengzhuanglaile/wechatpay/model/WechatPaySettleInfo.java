4c9e1fcc3f1c14a96ab6c2cfefe1d802eebf3ddf
沙与沫 <zdy410291479@gmail.com>
2022-09-26 17:08:51 +0800

    zdyu

--- src/main/java/com/zhengzhuanglaile/wechatpay/model/WechatPaySettleInfo.java
+++ src/main/java/com/zhengzhuanglaile/wechatpay/model/WechatPaySettleInfo.java
@@ -25,6 +25,9 @@ public class WechatPaySettleInfo {
         this.profitSharing = profitSharing;
     }
 
+    public WechatPaySettleInfo(){
+    }
+
     public WechatPaySettleInfo(boolean profitSharing){
         this.profitSharing = profitSharing;
     }
